7169edd6dde0522e856849fd2cd0eab7a6d807c2
danrix <danrix@10.0.0.5>
2014-05-08 20:29:24 -0400

    Internal (Sudoku Project): Main class with methods and functions

--- sudoku/src/sudoku/Sudoku.java
+++ sudoku/src/sudoku/Sudoku.java
@@ -6,6 +6,8 @@
 
 package sudoku;
 
+import java.util.Scanner;
+
 /**
  *
  * @author danrix
@@ -16,7 +18,23 @@ public class Sudoku {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Sudoku l_game = new Sudoku();
+        l_game.get_name();
+        l_game.display_help();
     }
+    
+    String name;
+    String instructions = "";
+    
+    public void get_name(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        this.name = input.next();
+        }
+    
+    public void display_help(){
+        System.out.println("\n Welcom " + this.name + "\n");
+        System.out.println(this.instructions);
+        }  
     
 }
