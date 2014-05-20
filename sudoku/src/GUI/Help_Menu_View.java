22e7cbc50d3a9576ddc63ba5a5e2f02890374648
danrix <danrix@10.0.0.5>
2014-05-19 22:05:45 -0400

    Internal (Sudoku Project): Added Help_Menu class that gives the messages to be displayed when chosen from the player using the Help_Menu_View class.

---
+++ sudoku/src/GUI/Help_Menu_View.java
@@ -0,0 +1,14 @@
+package GUI;
+
+/*
+Class Description: Representation of a 3x3 cell block on a sudoku game board.
+*/
+
+public class Help_Menu_View 
+{
+
+    /* For an example see the TicTacToe_L4After package that you can clone
+        the following repository: https://github.com/BYUI-OnlineCIT260/CIT260Examples
+        Check out HelpMenuView.java
+    */
+}
